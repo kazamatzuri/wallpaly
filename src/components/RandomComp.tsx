ed56096432362f41cb37f538a17b1946eaf85a17
Fab <Fabian.Haupt@plangrid.com>
2019-10-08 12:09:24 +0200

    tamed random button a bit

--- src/components/RandomComp.tsx
+++ src/components/RandomComp.tsx
@@ -14,10 +14,10 @@ function randomize(cb: any) {
   let r = {
     lineNumber: getRandomInt(280) + 20,
     seed: getRandomInt(9999),
-    anchorpoints: getRandomInt(295) + 5,
-    jitterX: getRandomInt(200),
-    jitterY: getRandomInt(200),
-    colorspread: getRandomInt(180) + 20,
+    anchorpoints: getRandomInt(30) + 30,
+    jitterX: getRandomInt(5),
+    jitterY: getRandomInt(30),
+    colorspread: getRandomInt(20) + 10,
     initialAmplitude: getRandomInt(280) + 20,
     color: {
       r: getRandomInt(255),
