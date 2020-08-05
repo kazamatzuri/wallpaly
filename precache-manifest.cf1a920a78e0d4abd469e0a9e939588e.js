f09869da6b959e93aa8808d6bc0f988fcc7a535b
kazamatzuri <fabian.haupt@autodesk.com>
2020-08-05 19:42:09 +0000

    Deploying to gh-pages from master fc1092f294d493e6ed736d18bb2b1f511bc5250d

---
+++ precache-manifest.cf1a920a78e0d4abd469e0a9e939588e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbdcf16b63a3b4b21d3089552458814c",
+    "url": "/wallpaly/index.html"
+  },
+  {
+    "revision": "43f3f66d8cc0b282606e",
+    "url": "/wallpaly/static/css/main.0b5e823f.chunk.css"
+  },
+  {
+    "revision": "f09ea3ae93249ff30ada",
+    "url": "/wallpaly/static/js/2.214747b6.chunk.js"
+  },
+  {
+    "revision": "43f3f66d8cc0b282606e",
+    "url": "/wallpaly/static/js/main.30af8035.chunk.js"
+  },
+  {
+    "revision": "93eb335d6cf71bdac1cc",
+    "url": "/wallpaly/static/js/runtime-main.071b86a8.js"
+  }
+]);
